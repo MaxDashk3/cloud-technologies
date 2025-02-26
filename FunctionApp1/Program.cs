a99fad6ee5aa4800359dabd847a6468079e78012
Maxim Dashkovskiy <maxdashk3@gmail.com>
2025-02-26 15:44:26 +0200

    added lab 4

---
+++ FunctionApp1/Program.cs
@@ -0,0 +1,21 @@
+using Azure;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+var builder = FunctionsApplication.CreateBuilder(args);
+builder.ConfigureFunctionsWebApplication();
+builder.Services.
+AddApplicationInsightsTelemetryWorkerService().
+ConfigureFunctionsApplicationInsights().
+AddAzureClients(b =>
+{
+    b.AddBlobServiceClient(Environment.GetEnvironmentVariable("blobConn"));
+    var endpoint = new Uri(Environment.
+    GetEnvironmentVariable("textAnalyticsEndpoint"));
+    var credential = new AzureKeyCredential(Environment.
+    GetEnvironmentVariable("textAnalyticsKey"));
+    b.AddTextAnalyticsClient(endpoint, credential);
+});
+builder.Build().Run();
